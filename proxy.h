371711e6c8070a1ec249bcd1e960c6f22b4d2921
ocfbnj <ocfbnj@qq.com>
2021-02-03 16:29:16 +0800

    add ss-remote (i.e server)

--- proxy.h
+++ proxy.h
@@ -14,11 +14,22 @@ struct client_proxy_context {
     struct cipher* de_cipher;
 };
 
+struct server_proxy_context {
+    struct bufferevent* out_bev;
+    struct cipher* en_cipher;
+};
+
 struct client_proxy_context* alloc_client_proxy_context(struct bufferevent* bev);
 void free_client_proxy_context(struct client_proxy_context* ctx);
 
+struct server_proxy_context* alloc_server_proxy_context(struct bufferevent* bev);
+void free_server_proxy_context(struct server_proxy_context* ctx);
+
 void client_proxy_read_cb(struct bufferevent* bev, void* ctx);
 void client_proxy_event_cb(struct bufferevent* bev, short events, void* ctx);
+
+void server_proxy_read_cb(struct bufferevent* bev, void* arg);
+void server_proxy_event_cb(struct bufferevent* bev, short events, void* arg);
 
 #ifdef __cplusplus
 }
